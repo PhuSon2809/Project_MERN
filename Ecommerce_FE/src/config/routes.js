4b44964c361b90ddd858b62f9e7e228a5959a891
Tran Phu Son <phuson2809@gmail.com>
2023-02-28 08:05:30 +0700

    project init

---
+++ Ecommerce_FE/src/config/routes.js
@@ -0,0 +1,10 @@
+const routes = {
+  home: "/",
+  detail: "/detail/:idBigProject/:id",
+  porfolios: "/porfolios",
+  myself: "/myself/:tag",
+  activities: "/activities",
+  notFound: "*",
+};
+
+export default routes;
